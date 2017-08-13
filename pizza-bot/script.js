8f85010d63facb0c5d5e21555ea716b412322ee6
blackhood5678 <yanyyanev@abv.bg>
2017-08-13 22:46:22 +0300

    test
    test

--- pizza-bot/script.js
+++ pizza-bot/script.js
@@ -17,16 +17,4 @@ $(document).ready(function(){
 		$('.msg_box').show();
 	});
 	
-	/*$('textarea').keypress(
-    function(e){
-        if (e.keyCode == 13) {
-            e.preventDefault();
-            var msg = $(this).val();
-			$(this).val('');
-			if(msg!='')
-			$('<div class="msg_b">'+msg+'</div>').insertBefore('.msg_push');
-			$('.msg_body').scrollTop($('.msg_body')[0].scrollHeight);
-        }
-    });*/
-	
 });
